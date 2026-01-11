7eac7d22001bad9603db3e4039f5201258353279
SadishNanayakkara <sswijerathnananayakkara@gmail.com>
2026-01-11 22:53:28 +0530

    Update server response message and add nodemon as a dev dependency

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("It's working");
+  res.send("It's working 123 fine");
 });
 
 app.listen(5001, () => {
